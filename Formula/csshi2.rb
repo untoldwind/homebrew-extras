e18561e38ab6911a870fd7669e7d0b09170259c2
Untoldwind <landru@untoldwind.net>
2014-12-03 13:25:41 +0100

    Initial

---
+++ Formula/csshi2.rb
@@ -0,0 +1,26 @@
+require "formula"
+
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                /usr/local/Library/Contributions/example-formula.rb
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Csshi2 < Formula
+  homepage "https://github.com/untoldwind/csshi2"
+  url "https://github.com/untoldwind/csshi2/archive/0.3.tar.gz"
+  sha1 "fafc09dfd4ba0a8e1960598fa7c13922871a92ff"
+
+  head "https://github.com/untoldwind/csshi2.git"
+
+  depends_on "zsh-completions" => :optional
+  depends_on "bash-completion" => :optional
+
+  def install
+  	bin.install "csshi2"
+    if build.with? "zsh-completions"
+      (share / "zsh-completions").install "zsh/_csshi2"
+    end
+    if build.with? "bash-completion"
+      (etc / "bash_completion.d").install "bash/csshi2"
+    end
+  end
+end
